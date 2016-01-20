3d77e37b98952291059ef414a2a381f671bec529
platonovAlexey <kalyawe4ka@gmail.com>
2016-01-20 15:36:42 +0300

    getting started, added header

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/html5shiv/dist/html5shiv.js
+//= ../../bower_components/html5shiv/dist/html5shiv-printshiv.js
+
+
+/*
+ * Custom
+ */
+//= partials/app.js
