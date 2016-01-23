70aa5acc9275d50e33226415a9bd576393b807bc
platonovAlexey <kalyawe4ka@gmail.com>
2016-01-24 01:47:48 +0300

    edit ie8, fonts

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
  */
 //= ../../bower_components/jquery/dist/jquery.js
 //= ../../bower_components/html5shiv/dist/html5shiv.js
-//= ../../bower_components/html5shiv/dist/html5shiv-printshiv.js
+
 
 
 /*
