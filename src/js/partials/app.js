1fa4f82a84f0d0ff4a0e9512c7d93533eaedf986
platonovAlexey <kalyawe4ka@gmail.com>
2016-01-26 18:51:29 +0300

    adaptation

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,1 +1,8 @@
 console.log("in app.js")
+$(".mob__nav").on('click',function(){
+      if($(".nav__header").hasClass('show__nav__header')){
+        $(".nav__header").removeClass('show__nav__header');
+      }else{
+        $(".nav__header").addClass('show__nav__header');
+      }
+    });
